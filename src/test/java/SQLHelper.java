9f978e3112a6c0980aed48477e6db013103ffe23
Anastasia I <niavolk@gmail.com>
2023-07-15 18:39:37 +0400

    Initial commit

---
+++ src/test/java/SQLHelper.java
@@ -0,0 +1,49 @@
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.ScalarHandler;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SQLHelper {
+    private static QueryRunner runner = new QueryRunner();
+    private static String url = System.getProperty("db.url");
+    private static String userDB = System.getProperty("app.userDB");
+    private static String password = System.getProperty("app.password");
+
+    private SQLHelper() {
+    }
+
+    private static Connection getConn() throws SQLException {
+        return DriverManager.getConnection(url, userDB, password);
+    }
+
+    public static void clearData() throws SQLException {
+        var conn = DriverManager.getConnection(url, userDB, password);
+        runner.update(conn, "DELETE FROM credit_request_entity;");
+        runner.update(conn, "DELETE FROM payment_entity;");
+        runner.update(conn, "DELETE FROM order_entity;");
+    }
+
+    public static Long getRecordsCountFromOrderEntity() {
+        var countSQL = "SELECT COUNT(*) FROM order_entity";
+        try (var conn = getConn()) {
+            var number = runner.query(conn, countSQL, new ScalarHandler<Long>());
+            return number;
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+        return null;
+    }
+
+    public static Long getRecordsCountFromPaymentEntity() {
+        var countSQL = "SELECT COUNT(*) FROM payment_entity";
+        try (var conn = getConn()) {
+            var number = runner.query(conn, countSQL, new ScalarHandler<Long>());
+            return number;
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+        return null;
+    }
+}
